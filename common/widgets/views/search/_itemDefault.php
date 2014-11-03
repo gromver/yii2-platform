4543be33445fd11dfbcca54f82ec41a97a42e89f
Roman <roman.tlt-84@mail.ru>
2014-11-03 10:50:41 +0400

    SearchResults date fix

--- common/widgets/views/search/_itemDefault.php
+++ common/widgets/views/search/_itemDefault.php
@@ -8,6 +8,6 @@ use yii\helpers\Html;
 echo Html::beginTag('div', ['class' => 'search-result-item']);
 
 echo Html::a($model->highlight['title'][0], $model->getViewLink(), ['class' => 'h4 title']);
-echo Html::tag('p', ($model->hasAttribute('date') ? Html::tag('small', Yii::$app->formatter->asDate($model->date, 'Y M d'), ['class' => 'date']) . ' - ' : '') . implode(' ... ', $model->highlight['text']), ['class' => 'text']);
+echo Html::tag('p', ($model->hasAttribute('date') ? Html::tag('small', Yii::$app->formatter->asDate($model->date, 'd MMMM Y'), ['class' => 'date']) . ' - ' : '') . implode(' ... ', $model->highlight['text']), ['class' => 'text']);
 
 echo Html::endTag('div');
